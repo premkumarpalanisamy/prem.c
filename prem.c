00f9a4f54468e6a7678ffbc0a3ae5475ee8c3c75
premkumarpalanisamy <31505455+premkumarpalanisamy@users.noreply.github.com>
2018-03-02 16:38:43 +0530

    Create prem.c

---
+++ prem.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void) {
+char a[50];
+int count=0;
+int i;
+scanf("%[^\n]s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+	
+	if(a[i]==' ')
+	{
+		count++;
+	}
+}
+	printf("%d",++count);
+	return 0;
+}
