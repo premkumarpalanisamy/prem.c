8b71366ff608bbf38a39077841e91ba842b183fe
premkumarpalanisamy <31505455+premkumarpalanisamy@users.noreply.github.com>
2018-03-12 16:08:13 +0530

    Create rpk8.c

---
+++ rpk8.c
@@ -0,0 +1,19 @@
+int main(void)
+{
+char ch[100];
+int i,k,count=0;
+gets(ch);
+for(k=0;ch[k]!='\0';++k)
+{
+	
+}
+if(ch>=1&&ch<=9)
+for(i=0;i<k;i++)
+{
+	
+	count++;
+}
+printf("%d",count);
+	return 0;
+}
+	
