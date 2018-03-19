36d5b5c705914aab6d8c61a8eb176d1169dc7514
premkumarpalanisamy <31505455+premkumarpalanisamy@users.noreply.github.com>
2018-03-19 16:09:56 +0530

    Create combaine string.c

---
+++ combaine string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include<string.h>
+
+int main(void) 
+{
+	char str1[40],str2[40];
+	int n1,n2;
+	printf("enter the string1:");
+	scanf("%s",str1);
+	printf("enter the string2:");
+	scanf("%s",str2);
+	printf("%s%s\n",str1,str2);
+	return 0;
+}
